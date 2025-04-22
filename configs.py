0c5d58f2297fab1cd6a5c059425d4d40ddc0dc5f
Rᴇxʏ sᴀᴍᴀ <jeffreymosesdj@gmail.com>
2025-04-22 21:50:55 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -10,7 +10,7 @@ import os
 class Config:
     API_ID = int(os.getenv("API_ID", "28744454"))
     API_HASH = os.getenv("API_HASH", "debd37cef0ad1a1ce45d0be8e8c3c5e7")
-    BOT_TOKEN = os.getenv("BOT_TOKEN", "7953776996:AAHKLreBz3nDUCEScfK3lNB8EMoSfvq4QxA")
+    BOT_TOKEN = os.getenv("BOT_TOKEN", "7773900178:AAEQEQAXahfYAVsol1EUcwYPKa0Or2cPCdg")
     
     # Force Subscribe Channel ID (Ensure bot is admin)
     CHID = int(os.getenv("CHID", "-1002410513772"))
