df27222a0bb5eb8c64c30b303c125b70037946a9
Rᴇxʏ sᴀᴍᴀ <jeffreymosesdj@gmail.com>
2025-04-23 16:14:56 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -14,6 +14,9 @@ class Config:
     
     # Force Subscribe Channel ID (Ensure bot is admin)
     CHID = int(os.getenv("CHID", "-1002410513772"))
+    
+    # Add your string session here
+    STRING_SESSION = os.getenv("STRING_SESSION", "BQG2mwYAgxsHqkyxZSAOLyQAb1EkvktN-w5rw-e_CInEL99lVDK2TzAAhn4DDd5hjlC4FqqGC0OSkkkBLXj6K3pnXF-IKNLbhAHx4U8QTttNb-MWZmnjBKgukjjk76u6uQc5McpplXy_yhHj6_QTh7SKL_nlY-daIl-nRIgl3v6OEP1WgSdmDS0dUAfrNbJ1WkuHfBpsKROcVatevupX6trdJONPaZwgdPrKOlfFEl4aAJItyPcj5_lnAkfBC9kn7xWl-dEo-xi51QjCDCJ0hFsXxC-vRHqr-YMxR6uAsdc0tWlAdVDG-2SKx4Mqz7XhTZGeKkVYcj4Gp6kxpvVmTYeK-jdibQAAAAF1g6UNAA")
 
     # Admin User IDs
     SUDO = list(map(int, os.getenv("SUDO", "5548954124 6429532957").split()))
