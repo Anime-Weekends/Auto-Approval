0605c54ac14cbe8b74467d70efd12e0fa9f46f84
Rᴇxʏ sᴀᴍᴀ <jeffreymosesdj@gmail.com>
2025-04-23 13:24:30 +0530

    Update database.py

--- database.py
+++ database.py
@@ -15,17 +15,15 @@ def already_db(user_id):
     return bool(users.find_one({"user_id": str(user_id)}))
 
 def add_user(user_id):
-    if already_db(user_id):
-        return
-    users.insert_one({
-        "user_id": str(user_id),
-        "joined_at": datetime.utcnow().isoformat()
-    })
+    if not already_db(user_id):
+        users.insert_one({
+            "user_id": str(user_id),
+            "joined_at": datetime.utcnow().isoformat()
+        })
 
 def remove_user(user_id):
-    if not already_db(user_id):
-        return
-    users.delete_one({"user_id": str(user_id)})
+    if already_db(user_id):
+        users.delete_one({"user_id": str(user_id)})
 
 def all_users():
     return users.count_documents({})
@@ -36,12 +34,11 @@ def already_dbg(chat_id):
     return bool(groups.find_one({"chat_id": str(chat_id)}))
 
 def add_group(chat_id):
-    if already_dbg(chat_id):
-        return
-    groups.insert_one({
-        "chat_id": str(chat_id),
-        "joined_at": datetime.utcnow().isoformat()
-    })
+    if not already_dbg(chat_id):
+        groups.insert_one({
+            "chat_id": str(chat_id),
+            "joined_at": datetime.utcnow().isoformat()
+        })
 
 def all_groups():
     return groups.count_documents({})
@@ -60,7 +57,6 @@ def remove_admin_db(user_id):
 
 def list_admins_db():
     return [admin["user_id"] for admin in admins.find()]
-
 
 # === Sudo Checker ===
 from pyrogram import filters
