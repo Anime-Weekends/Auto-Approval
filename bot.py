5cf0c69dbdca58ac2f3ef00855bc24c914d7ce86
Rᴇxʏ sᴀᴍᴀ <jeffreymosesdj@gmail.com>
2025-04-23 22:20:44 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,6 +14,7 @@ from pyrogram.enums import ChatMemberStatus, ParseMode
 from pyrogram.errors import UserDeactivated, UserBlocked
 from pyrogram.errors import PeerIdInvalid
 from pyrogram.errors import RPCError
+from your_database_file import log_approval, get_total_approvals
 
 from database import add_user, add_group, all_users, all_groups, users, remove_user
 from database import add_admin_db, remove_admin_db, list_admins_db, is_admin
