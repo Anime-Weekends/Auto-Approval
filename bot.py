a4a87645ce8f29b0484347c1627b61d159a5d0dc
Rᴇxʏ sᴀᴍᴀ <jeffreymosesdj@gmail.com>
2025-04-24 02:41:45 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -206,11 +206,11 @@ async def dbtool(_, m: Message):
     timestamp = datetime.now().strftime("%d %b %Y • %I:%M %p")
 
     caption = (
-        f"**<blockquote>📊 Cʜᴀᴛ sᴛᴀᴛɪsᴛɪᴄs</blockquote>**\n\n"
-        f"<blockquote>👤 ᴜsᴇʀs : `{total_users}` ({user_percent:.1f}%)</blockquote>\n"
-        f"<blockquote>👥 ɢʀᴏᴜᴘs : `{total_groups}` ({group_percent:.1f}%)</blockquote>\n"
-        f"<blockquote>📦 ᴛᴏᴛᴀʟ ᴄʜᴀᴛs : `{total}`</blockquote>\n\n"
-        f"<blockquote>🕒 ʟᴀsᴛ ᴜᴘᴅᴀᴛᴇᴅ : `{timestamp}`</blockquote>"
+        f"**<blockquote>➥ Cʜᴀᴛ sᴛᴀᴛɪsᴛɪᴄs</blockquote>**\n\n"
+        f"<blockquote>❏ ᴜsᴇʀs : `{total_users}` ({user_percent:.1f}%)</blockquote>\n"
+        f"<blockquote>❏ ɢʀᴏᴜᴘs : `{total_groups}` ({group_percent:.1f}%)</blockquote>\n"
+        f"<blockquote>❏ ᴛᴏᴛᴀʟ ᴄʜᴀᴛs : `{total}`</blockquote>\n\n"
+        f"<blockquote>➥ ʟᴀsᴛ ᴜᴘᴅᴀᴛᴇᴅ : `{timestamp}`</blockquote>"
     )
 
     await m.reply_photo(
