104e0fb658a9d861823ae2d7fd9a42223e61d328
Rᴇxʏ sᴀᴍᴀ <jeffreymosesdj@gmail.com>
2025-04-25 12:56:26 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -11,7 +11,7 @@ class Config:
     API_ID = int(os.getenv("API_ID", "28744454"))
     API_HASH = os.getenv("API_HASH", "debd37cef0ad1a1ce45d0be8e8c3c5e7")
     BOT_TOKEN = os.getenv("BOT_TOKEN", "7773900178:AAEQEQAXahfYAVsol1EUcwYPKa0Or2cPCdg")
-    BOT_USERNAME = os.getenv("BOT_USERNAME", "Private_Auto_Approval_Bot")
+    BOT_USERNAME = os.getenv("BOT_USERNAME", "AutoAnimeNews_Bot")
 
     
     # Add your string session here
