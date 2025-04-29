fcaaa16a5856331d64ed93b1b755379ece67142b
Rᴇxʏ sᴀᴍᴀ <jeffreymosesdj@gmail.com>
2025-04-29 17:13:20 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -10,8 +10,8 @@ import os
 class Config:
     API_ID = int(os.getenv("API_ID", "28744454"))
     API_HASH = os.getenv("API_HASH", "debd37cef0ad1a1ce45d0be8e8c3c5e7")
-    BOT_TOKEN = os.getenv("BOT_TOKEN", "7773900178:AAEQEQAXahfYAVsol1EUcwYPKa0Or2cPCdg")
-    BOT_USERNAME = os.getenv("BOT_USERNAME", "AutoAnimeNews_Bot")
+    BOT_TOKEN = os.getenv("BOT_TOKEN", "7953776996:AAG_RY59SemsMdCVgA4ggzwWFwt4fBN4V1c")
+    BOT_USERNAME = os.getenv("BOT_USERNAME", "Private_Auto_Approval_Bot")
 
     
     # Add your string session here
@@ -21,7 +21,7 @@ class Config:
     FORCE_SUB_CHANNELS = list(map(int, os.getenv("FORCE_SUB_CHANNELS", "-1002410513772 -1002296091847").split()))
     
     # Admin User IDs
-    SUDO = list(map(int, os.getenv("SUDO", "5548954124 6429532957").split()))
+    SUDO = list(map(int, os.getenv("SUDO", "5548954124 6429532957 6266529037").split()))
     
     # MongoDB URI
     MONGO_URI = os.getenv(
