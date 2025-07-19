825aaefdf6b714daed66837ba6717d78fb7e2648
Rᴇxʏ sᴀᴍᴀ <jeffreymosesdj@gmail.com>
2025-07-19 21:32:45 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -15,7 +15,7 @@ class Config:
 
     
     # Add your string session here
-    STRING_SESSION = os.getenv("STRING_SESSION", "BQCak1gAa4ArrhofSrskJiuShYW5QbYd1h-NIS_9w5IoMM7kL5rtVf8KOnT2IVFfdvSsz1sHnXh8ZRPYo3m2SMCQiu3uMThakFf9hCEygPIqTp9s-n5hfFrhryZ1ExC7SH2lPdA3NK5tA0pyh4gLTWiXd4M-PQACCaotkTW3ibe0VSHCZ0699bhiXdAFY3Oa0A40-mA83QpZqNqCZLJuN8SQbwioWh36x9-AEXBZ_rElBquXBSlJHeZv-DcYZChmfRI-FufzWtx-jCZcMKg_HwMMdNuwNH4eMC0Hz5oaBPP9ucwAFe4B1U9hjfXRYsF1etNemQrFzcVCdusfeyfpxuI6XizzoQAAAAGmk-HvAA")
+    STRING_SESSION = os.getenv("STRING_SESSION", "AQFUFTEArQJhO3y3jpH5-sgcOOQdaAXnh-8KfTyxhcED9k-qGMKyEfZs0hhMWxo8EV7pPDArvibarX51Z1tP6hDiWBIWvp_uvUL2IueKc2c7LpetB8VxN-Ur8RlytLEGF823VbDzxW5X3SLiWoji53AWsWE2VoF_2AeWvuiRfZ_ynfFBDy8EJWw0HW7DLnfsXiKwPFf58p5ah_baGvL25qhnr0xZEuJuZtBd80q_N8wC_mvL4NFbWNT9LLF-Ym3EUBqOIcFEiTK0aMvKwNgaF5sK7NNgX3NvlNqxv9ti_xNrEdeY1dztKKquP8xuC3_QDJc80V485Qa3uRHQe49C52hLoPk3zwAAAAHb5ZnEAA")
 
      # Force Subscribe Channel ID (Ensure bot is admin)
     FORCE_SUB_CHANNELS = list(map(int, os.getenv("FORCE_SUB_CHANNELS", "-1002410513772 -1002296091847").split()))
